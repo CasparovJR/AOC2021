5c8d44a30ee86a65f40228f10ded717691c9e5d4
Sam <smer0005@student.monash.edu>
2021-12-08 00:15:52 +1100

    made reliable part 2 smaller because smaller is better

--- Chal07/Chal07Part2(reliable).py
+++ Chal07/Chal07Part2(reliable).py
@@ -2,5 +2,4 @@ import statistics as s
 import math
 def sumCalc(mean): return sum(abs(x-mean) * (abs(x-mean)+1)//2 for x in crabList)
 crabList = [int(x) for x in open("Chal07.txt", 'r').read().strip().split(',')]
-meanFloor, meanCeil = math.floor(s.mean(crabList)), math.ceil(s.mean(crabList))
-print(min(sumCalc(meanFloor), sumCalc(meanCeil)))
+print(min(sumCalc(math.floor(s.mean(crabList))), sumCalc(math.ceil(s.mean(crabList)))))
